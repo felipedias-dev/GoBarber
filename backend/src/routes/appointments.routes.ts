e6740956c637e4031451c04805856c786d76dc0b
Felipe Dias <felipedias.dev@gmail.com>
2020-08-28 13:44:05 -0300

    Add backend basic code

---
+++ backend/src/routes/appointments.routes.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointment.service';
+
+const appointmentRouter = Router();
+const appointmentsRepository = new AppointmentRepository();
+
+appointmentRouter.get('/', (request, response) => {
+  const appointment = appointmentsRepository.all();
+
+  return response.json(appointment);
+});
+
+appointmentRouter.post('/', (request, response) => {
+  try {
+    const { provider, date } = request.body;
+    const parsedDate = parseISO(date);
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+    const appointment = createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentRouter;
