9c937769bb62ba0f470d96564b708e7b92313910
Felipe Dias <felipedias.dev@gmail.com>
2020-08-29 23:56:54 -0300

    Add CreateAppointments migration

--- backend/src/database/migrations/1598647196036-CreateAppointments.ts
+++ backend/src/database/migrations/1598647196036-CreateAppointments.ts
@@ -3,6 +3,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 export default class CreateAppointments1598647196036
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
     await queryRunner.createTable(
       new Table({
         name: 'appointments',
@@ -12,16 +13,25 @@ export default class CreateAppointments1598647196036
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
           },
         ],
       }),
