d50ffa2fc9347b8a1698dfdac2a0b3a8f91d7a91
Felipe Dias <felipedias.dev@gmail.com>
2020-09-01 00:14:30 -0300

    Update service AuthenticateUserService to use authConfig file

--- backend/src/services/AuthenticateUser.service.ts
+++ backend/src/services/AuthenticateUser.service.ts
@@ -3,6 +3,7 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
+import authConfig from '../config/auth';
 
 interface Request {
   email: string;
@@ -29,9 +30,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination!');
     }
 
-    const token = sign({}, 'c3c3e9a89946db4b463c4a4c09a2c0f2', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return { user, token };
