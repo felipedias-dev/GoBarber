e2160eb1853d4d9b33f0f62a5758b0a075975882
Felipe Dias <felipedias.dev@gmail.com>
2020-09-01 18:44:35 -0300

    Add auth middleware to users route

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,21 +1,40 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUser.service';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
+import UpdateUserAvatarService from '../services/UpdateUserAvatar.service';
 
-const userRouter = Router();
+const usersRouter = Router();
+const upload = multer(uploadConfig);
 
-userRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
-    const createUser = new CreateUserService();
-    const user = await createUser.execute({ name, email, password });
+usersRouter.post('/', async (request, response) => {
+  const { name, email, password } = request.body;
+  const createUser = new CreateUserService();
+  const user = await createUser.execute({ name, email, password });
+
+  delete user.password;
+
+  return response.json(user);
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
     delete user.password;
 
     return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
+  },
+);
 
-export default userRouter;
+export default usersRouter;
