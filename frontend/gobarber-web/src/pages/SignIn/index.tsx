d75d39016bf4d288a38e1548197de6c527b4943f
felipedias-dev <felipedias.dev@gmail.com>
2021-01-23 17:30:45 -0300

    Fix SignIn page format

--- frontend/gobarber-web/src/pages/SignIn/index.tsx
+++ frontend/gobarber-web/src/pages/SignIn/index.tsx
@@ -9,13 +9,19 @@ const SignIn: React.FC = () => (
   <Container>
     <Content>
       <img src={logoImg} alt="GoBarber" />
+
       <form>
         <h1>Faça seu logon</h1>
+
         <input type="text" placeholder="E-mail" />
+
         <input type="password" placeholder="Senha" />
+
         <button type="submit">Entrar</button>
+
         <a href="forgot">Esqueci minha senha</a>
       </form>
+
       <a href="signup">
         <FiLogIn />
         Criar conta
