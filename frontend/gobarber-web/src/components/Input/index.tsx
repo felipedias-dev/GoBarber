d73f1e14c6ef72000264994d7e8fffbc23923121
felipedias-dev <felipedias.dev@gmail.com>
2021-01-24 00:14:02 -0300

    Add Input component

---
+++ frontend/gobarber-web/src/components/Input/index.tsx
@@ -0,0 +1,18 @@
+import React, { InputHTMLAttributes } from 'react';
+import { IconType } from 'react-icons';
+
+import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: IconType;
+}
+
+const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
+  <Container>
+    {Icon && <Icon size={20} />}
+    <input {...rest} />
+  </Container>
+);
+
+export default Input;
