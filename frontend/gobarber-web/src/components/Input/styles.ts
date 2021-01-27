a1d6bd9f1213970a489f80284ff5cb9b75716546
felipedias-dev <felipedias.dev@gmail.com>
2021-01-26 23:34:48 -0300

    Update style for Container to handle focus and blur

--- frontend/gobarber-web/src/components/Input/styles.ts
+++ frontend/gobarber-web/src/components/Input/styles.ts
@@ -1,6 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: Boolean;
+  isFilled: Boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   height: 56px;
   background: #232129;
   border-radius: 10px;
@@ -14,6 +19,19 @@ export const Container = styled.div`
   & + div {
     margin-top: 8px;
   }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
     color: #f4ede8;
