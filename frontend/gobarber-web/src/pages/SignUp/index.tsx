22117b484db7c5cb63fa84f4671fa59605e041b6
felipedias-dev <felipedias.dev@gmail.com>
2021-01-28 10:10:34 -0300

    Update form to show input errors coming from Yup validation

--- frontend/gobarber-web/src/pages/SignUp/index.tsx
+++ frontend/gobarber-web/src/pages/SignUp/index.tsx
@@ -1,17 +1,22 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
 
 import logoImg from '../../assets/logo.svg';
-
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-
 import { Container, Content, Background } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  console.log(formRef);
+
   const handleSubmit = useCallback(async (data: object) => {
+    formRef.current?.setErrors({});
+
     try {
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
@@ -24,8 +29,10 @@ const SignUp: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false,
       });
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+      console.log(err);
     }
   }, []);
 
@@ -35,7 +42,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastrro</h1>
 
           <Input name="name" type="text" icon={FiUser} placeholder="Nome" />
