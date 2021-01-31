e62eecaf99cc5d24940e9da7ac2e95f43290ccc7
felipedias-dev <felipedias.dev@gmail.com>
2021-01-31 01:47:30 -0300

    Update Toast component isolation

--- frontend/gobarber-web/src/components/ToastContainer/index.tsx
+++ frontend/gobarber-web/src/components/ToastContainer/index.tsx
@@ -1,46 +1,19 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
-import { Container, Toast } from './styles';
+import { Container } from './styles';
+import { ToastMessage } from '../../hooks/toast';
+import Toast from './Toast';
 
-const ToastContainer: React.FC = () => {
+interface ToastContainerProps {
+  messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
   return (
     <Container>
-      <Toast>
-        <FiAlertCircle size={20} />
-
-        <div>
-          <strong>Aviso importante</strong>
-          <p>Agora pode fazer login com a digital</p>
-        </div>
-
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
-      <Toast type="success">
-        <FiAlertCircle size={20} />
-
-        <div>
-          <strong>Cadastro realizado com sucesso</strong>
-        </div>
-
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
-      <Toast type="error">
-        <FiAlertCircle size={20} />
-
-        <div>
-          <strong>Aconteceu um erro</strong>
-          <p>Não foi possível fazer login na aplicação</p>
-        </div>
-
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
+      {messages.map(message => (
+        <Toast key={message.id} message={message} />
+      ))}
     </Container>
   );
 };
