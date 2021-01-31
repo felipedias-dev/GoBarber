a98bcfa88c21c09d7bfd6d1bdde541a10ac1daf4
felipedias-dev <felipedias.dev@gmail.com>
2021-01-31 01:47:49 -0300

    Update style for Toast component

--- frontend/gobarber-web/src/components/ToastContainer/styles.ts
+++ frontend/gobarber-web/src/components/ToastContainer/styles.ts
@@ -1,8 +1,4 @@
-import styled, { css } from 'styled-components';
-
-interface ToastProps {
-  type?: 'info' | 'success' | 'error';
-}
+import styled from 'styled-components';
 
 export const Container = styled.div`
   position: absolute;
@@ -10,61 +6,4 @@ export const Container = styled.div`
   right: 0;
   padding: 30px;
   overflow: hidden;
-`;
-
-const toastTypes = {
-  info: css`
-    background: #ebf8ff;
-    color: #3172b7;
-  `,
-
-  success: css`
-    background: #b6fffa;
-    color: #2e8f6a;
-  `,
-
-  error: css`
-    background: #fddede;
-    color: #c53030;
-  `,
-};
-
-export const Toast = styled.div<ToastProps>`
-  width: 360px;
-  position: relative;
-  padding: 16px 30px 16px 16px;
-  border-radius: 10px;
-  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
-  display: flex;
-
-  & + div {
-    margin-top: 8px;
-  }
-
-  ${props => toastTypes[props.type || 'info']}
-
-  > svg {
-    margin-right: 12px;
-    align-self: center;
-  }
-
-  div {
-    flex: 1;
-
-    p {
-      margin-top: 4px;
-      font-size: 14px;
-      opacity: 0.8;
-      line-height: 20px;
-    }
-  }
-
-  button {
-    position: absolute;
-    top: 8px;
-    right: 8px;
-    border: 0;
-    background: transparent;
-    color: inherit;
-  }
 `;
